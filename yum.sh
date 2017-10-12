8bee8973eafbeee4d93b1a013e8f5fcbd7f0ff25
12345bt <31231177+12345bt@users.noreply.github.com>
2017-10-12 11:46:45 +0800

    Update yum.sh

--- yum.sh
+++ yum.sh
@@ -5,5 +5,6 @@ echo -e "nameserver 114.114.114.114\nnameserver 8.8.8.8" >> /etc/resolv.conf
 #一键更换中科院yum源
 wget -O /etc/yum.repos.d/CentOS-Base.repo https://12345bt.github.io/centos/CentOS-Base.repo
 wget -qO /etc/yum.repos.d/epel.repo https://12345bt.github.io/centos/epel-7.repo
-yum clean all
+yum clean metadata
 yum makecache
+yum -y update
