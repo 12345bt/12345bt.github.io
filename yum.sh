70818a82270076916e48c6186400aa2a23d05fa7
12345bt <31231177+12345bt@users.noreply.github.com>
2017-10-12 12:58:00 +0800

    Update yum.sh

--- yum.sh
+++ yum.sh
@@ -4,8 +4,10 @@
 echo -e "nameserver 114.114.114.114\nnameserver 8.8.8.8" >> /etc/resolv.conf
 #一键更换中科院yum源
 cd /etc/yum.repos.d
-mv CentOS-Base.repo CentOS-Base.repo.bk
+mv CentOS-Base.repo CentOS-Base.repo.backup
+mv epel.repo epel.repo.backup
+mv epel-testing.repo epel-testing.repo.backup
 wget https://12345bt.github.io/centos/CentOS-Base.repo
-wget -qO /etc/yum.repos.d/epel.repo https://12345bt.github.io/centos/epel-7.repo
+wget https://12345bt.github.io/centos/epel-7.repo
 yum clean metadata
 yum makecache
