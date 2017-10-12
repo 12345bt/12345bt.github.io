e58254fca2c4d08b66d0ac00e5daaaff0a570db6
12345bt <31231177+12345bt@users.noreply.github.com>
2017-10-12 12:32:12 +0800

    Update yum.sh

--- yum.sh
+++ yum.sh
@@ -3,7 +3,9 @@
 #更换国内Dns
 echo -e "nameserver 114.114.114.114\nnameserver 8.8.8.8" >> /etc/resolv.conf
 #一键更换中科院yum源
-wget -O /etc/yum.repos.d/CentOS-Base.repo https://12345bt.github.io/centos/CentOS-Base.repo
+cd /etc/yum.repos.d
+mv CentOS-Base.repo CentOS-Base.repo.bk
+wget https://12345bt.github.io/centos/CentOS-Base.repo
 wget -qO /etc/yum.repos.d/epel.repo https://12345bt.github.io/centos/epel-7.repo
 yum clean metadata
 yum makecache
