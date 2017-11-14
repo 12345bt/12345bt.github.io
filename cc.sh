a6bcd467278f02b9525d8d91e386c1ce324db86c
12345bt <31231177+12345bt@users.noreply.github.com>
2017-11-14 21:38:06 +0800

    Update cc.sh

--- cc.sh
+++ cc.sh
@@ -1,3 +1,3 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 wget -c --no-check-certificate https://raw.githubusercontent.com/12345bt/12345bt.github.io/master/webbench-1.5.tar.gz && tar zxvf webbench-1.5.tar.gz && rm -rf webbench-1.5.tar.gz && cd webbench-1.5 && make && make install
