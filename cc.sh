17d32771767badd2eb6be0f70a825a0eece42bad
12345bt <31231177+12345bt@users.noreply.github.com>
2017-11-14 21:35:22 +0800

    Update cc.sh

--- cc.sh
+++ cc.sh
@@ -3,4 +3,6 @@
 #  webbench -c 5000 -t 120  http://www.baidu.com/
 
 yum install -y gcc gcc-c++ git
-wget -c --no-check-certificate https://raw.githubusercontent.com/12345bt/12345bt.github.io/master/webbench-1.5.tar.gz && tar zxvf webbench-1.5.tar.gz && rm -rf webbench-1.5.tar.gz && cd webbench-1.5 && make && make install
+wget -c --no-check-certificate https://raw.githubusercontent.com/12345bt/12345bt.github.io/master/webbench-1.5.tar.gz && tar zxvf webbench-1.5.tar.gz && rm -rf webbench-1.5.tar.gz && cd webbench-1.5
+make
+make install 
