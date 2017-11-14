8b4969b8ee1cd1a7885f426237920a99848a4c32
12345bt <31231177+12345bt@users.noreply.github.com>
2017-11-14 21:29:03 +0800

    Update cc.sh

--- cc.sh
+++ cc.sh
@@ -1,9 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 #  自用一键cc 安装脚本   使用方法 
 #  webbench -c 5000 -t 120  http://www.baidu.com/
 
-yum install -y gcc gcc-c++ git 
-wget -N --no-check-certificate 12345bt.github.io/webbench-1.5.tar.gz 
-tar zxvf webbench-1.5.tar.gz 
-cd webbench-1.5 
-make && make install 
+yum install -y gcc gcc-c++ git
+wget -c --no-check-certificate https://raw.githubusercontent.com/12345bt/12345bt.github.io/master/webbench-1.5.tar.gz && tar zxvf webbench-1.5.tar.gz && rm -rf webbench-1.5.tar.gz && cd webbench-1.5 && make && make install
