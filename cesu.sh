5daf8521825805b111f5f470358f599e4be51860
12345bt <31231177+12345bt@users.noreply.github.com>
2017-11-15 09:38:39 +0800

    Update cesu.sh

--- cesu.sh
+++ cesu.sh
@@ -1,15 +1,71 @@
 #!/usr/bin/env bash
-
-if  [ ! -e '/usr/bin/wget' ]; then
-    echo "Error: wget command not found. You must be install wget command at first."
-    exit 1
-fi
+#
+# Description: Auto test download & I/O speed & network to China script
+#
+# Copyright (C) 2017 - 2017 Oldking <oooldking@gmail.com>
+#
+# Thanks: Bench.sh <i@teddysun.com>
+#
+# URL: https://www.oldking.net/350.html
+#
 
 # Colors
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
+SKYBLUE='\033[0;36m'
 PLAIN='\033[0m'
+
+# check release
+if [ -f /etc/redhat-release ]; then
+    release="centos"
+elif cat /etc/issue | grep -Eqi "debian"; then
+    release="debian"
+elif cat /etc/issue | grep -Eqi "ubuntu"; then
+    release="ubuntu"
+elif cat /etc/issue | grep -Eqi "centos|red hat|redhat"; then
+    release="centos"
+elif cat /proc/version | grep -Eqi "debian"; then
+    release="debian"
+elif cat /proc/version | grep -Eqi "ubuntu"; then
+    release="ubuntu"
+elif cat /proc/version | grep -Eqi "centos|red hat|redhat"; then
+    release="centos"
+fi
+
+# check root
+[[ $EUID -ne 0 ]] && echo -e "${RED}Error:${PLAIN} This script must be run as root!" && exit 1
+
+# check python
+if  [ ! -e '/usr/bin/python' ]; then
+        echo -e
+        read -p "${RED}Error:${PLAIN} python is not install. You must be install python command at first.\nDo you want to install? [y/n]" is_install
+        if [[ ${is_install} == "y" || ${is_install} == "Y" ]]; then
+            if [ "${release}" == "centos" ]; then
+                        yum -y install python
+                else
+                        apt-get -y install python
+                fi
+        else
+            exit
+        fi
+        
+fi
+
+# check wget
+if  [ ! -e '/usr/bin/wget' ]; then
+        echo -e
+        read -p "${RED}Error:${PLAIN} wget is not install. You must be install wget command at first.\nDo you want to install? [y/n]" is_install
+        if [[ ${is_install} == "y" || ${is_install} == "Y" ]]; then
+                if [ "${release}" == "centos" ]; then
+                        yum -y install wget
+                else
+                        apt-get -y install wget
+                fi
+        else
+                exit
+        fi
+fi
 
 get_opsy() {
     [ -f /etc/redhat-release ] && awk '{print ($1,$3~/^[0-9]/?$3:$4)}' /etc/redhat-release && return
@@ -22,7 +78,7 @@ next() {
 }
 
 io_test() {
-    (LANG=C dd if=/dev/zero of=test_$$ bs=64k count=16k conv=fdatasync && rm -f test_$$ ) 2>&1 | awk -F, '{io=$NF} END { print io}' | sed 's/^[ \t]*//;s/[ \t]*$//'
+    (LANG=C dd if=/dev/zero of=test_$$ bs=$1 count=$2 conv=fdatasync && rm -f test_$$ ) 2>&1 | awk -F, '{io=$NF} END { print io}' | sed 's/^[ \t]*//;s/[ \t]*$//'
 }
 
 calc_disk() {
@@ -47,38 +103,42 @@ tram=$( free -m | awk '/Mem/ {print $2}' )
 uram=$( free -m | awk '/Mem/ {print $3}' )
 swap=$( free -m | awk '/Swap/ {print $2}' )
 uswap=$( free -m | awk '/Swap/ {print $3}' )
-up=$( awk '{a=$1/86400;b=($1%86400)/3600;c=($1%3600)/60} {printf("%d days, %d hour %d min\n",a,b,c)}' /proc/uptime )
+up=$( awk '{a=$1/86400;b=($1%86400)/3600;c=($1%3600)/60} {printf("%d days %d hour %d min\n",a,b,c)}' /proc/uptime )
 load=$( w | head -1 | awk -F'load average:' '{print $2}' | sed 's/^[ \t]*//;s/[ \t]*$//' )
 opsy=$( get_opsy )
 arch=$( uname -m )
 lbit=$( getconf LONG_BIT )
 kern=$( uname -r )
-ipv6=$( wget -qO- -t1 -T2 ipv6.icanhazip.com )
 disk_size1=($( LANG=C df -hPl | grep -wvE '\-|none|tmpfs|devtmpfs|by-uuid|chroot|Filesystem' | awk '{print $2}' ))
 disk_size2=($( LANG=C df -hPl | grep -wvE '\-|none|tmpfs|devtmpfs|by-uuid|chroot|Filesystem' | awk '{print $3}' ))
 disk_total_size=$( calc_disk ${disk_size1[@]} )
 disk_used_size=$( calc_disk ${disk_size2[@]} )
+ptime=$(power_time)
+
 
 clear
 next
-echo "CPU model            : $cname"
-echo "Number of cores      : $cores"
-echo "CPU frequency        : $freq MHz"
-echo "Total size of Disk   : $disk_total_size GB ($disk_used_size GB Used)"
-echo "Total amount of Mem  : $tram MB ($uram MB Used)"
-echo "Total amount of Swap : $swap MB ($uswap MB Used)"
-echo "System uptime        : $up"
-echo "Load average         : $load"
-echo "OS                   : $opsy"
-echo "Arch                 : $arch ($lbit Bit)"
-echo "Kernel               : $kern"
+echo -e "发行版本     : ${SKYBLUE}$opsy${PLAIN}"
+echo -e "系统类型     : ${SKYBLUE}$arch ($lbit Bit)${PLAIN}"
+echo -e "内核版本     : ${SKYBLUE}$kern${PLAIN}"
+echo -e "CPU 型号     : ${SKYBLUE}$cname${PLAIN}"
+echo -e "CPU 核心     : ${SKYBLUE}$cores${PLAIN} 颗"
+echo -e "CPU 频率     : ${SKYBLUE}$freq MHz${PLAIN}"
+echo -e "硬盘大小     : ${SKYBLUE}$disk_total_size GB ($disk_used_size GB Used)${PLAIN}"
+echo -e "内存大小     : ${SKYBLUE}$tram MB ($uram MB Used)${PLAIN}"
+echo -e "交换空间     : ${SKYBLUE}$swap MB ($uswap MB Used)${PLAIN}"
+echo -e "运行时间      : ${SKYBLUE}$up${PLAIN}"
+echo -e "当前负载     : ${SKYBLUE}$load${PLAIN}"
 next
-io1=$( io_test )
-echo "I/O speed(1st run)   : $io1"
-io2=$( io_test )
-echo "I/O speed(2nd run)   : $io2"
-io3=$( io_test )
-echo "I/O speed(3rd run)   : $io3"
+echo -n "I/O speed( 32M )     : "
+io1=$( io_test 32k 1k )
+echo -e "${YELLOW}$io1${PLAIN}"
+echo -n "I/O speed( 256M )    : "
+io2=$( io_test 64k 4k )
+echo -e "${YELLOW}$io2${PLAIN}"
+echo -n "I/O speed( 2G )      : "
+io3=$( io_test 64k 32k )
+echo -e "${YELLOW}$io3${PLAIN}"
 ioraw1=$( echo $io1 | awk 'NR==1 {print $1}' )
 [ "`echo $io1 | awk 'NR==1 {print $2}'`" == "GB/s" ] && ioraw1=$( awk 'BEGIN{print '$ioraw1' * 1024}' )
 ioraw2=$( echo $io2 | awk 'NR==1 {print $1}' )
@@ -87,5 +147,5 @@ ioraw3=$( echo $io3 | awk 'NR==1 {print $1}' )
 [ "`echo $io3 | awk 'NR==1 {print $2}'`" == "GB/s" ] && ioraw3=$( awk 'BEGIN{print '$ioraw3' * 1024}' )
 ioall=$( awk 'BEGIN{print '$ioraw1' + '$ioraw2' + '$ioraw3'}' )
 ioavg=$( awk 'BEGIN{printf "%.1f", '$ioall' / 3}' )
-echo "Average I/O speed    : $ioavg MB/s"
+echo -e "Average I/O speed    : ${YELLOW}$ioavg MB/s${PLAIN}"
 next
