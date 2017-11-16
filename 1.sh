79a8b7563e9f90af5e392a3d66d0e656f7a99c84
12345bt <31231177+12345bt@users.noreply.github.com>
2017-11-16 10:27:53 +0800

    Create 1.sh

---
+++ 1.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+#傻瓜式一键安装 webbench 神器
+
+
+#安装必备技能
+yum -y install git gcc ctags
+
+#git克隆webbench
+git clone https://github.com/12345bt/webbench.git
+
+#防止出错 提前创建文件夹
+mkdir -m 777 -p /usr/local/man/man1
+
+#克隆ok 进入项目文件夹
+cd webbench
+
+#安装
+make && make install
