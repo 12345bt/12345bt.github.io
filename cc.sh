81ab5056454416237fcb6300a860d9b4952716cf
12345bt <31231177+12345bt@users.noreply.github.com>
2017-11-16 10:55:40 +0800

    Delete cc.sh

--- cc.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-wget -c --no-check-certificate https://raw.githubusercontent.com/12345bt/12345bt.github.io/master/webbench-1.5.tar.gz && tar zxvf webbench-1.5.tar.gz && rm -rf webbench-1.5.tar.gz && cd webbench-1.5
-make
-make install
